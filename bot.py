03e1839dba8ee5a0ee1c2645f2635c4c09e1a657
byomkesh462 <88849299+byomkesh462@users.noreply.github.com>
2022-02-26 17:50:06 +0530

    changed hit.php. to hit2.php

--- bot.py
+++ bot.py
@@ -61,7 +61,7 @@ async def link_handler(bot, message):
 
 
 async def get_shortlink(link):
-    url = f'https://hcitv.herokuapp.com/hit.php?url={link}'
+    url = f'https://hcitv.herokuapp.com/hit2.php?url={link}'
     #params = {'api': API_KEY, 'url': link}
 
     async with aiohttp.ClientSession() as session:
